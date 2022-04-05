e67600e81829fd99290d53af670c2e06dee9a8ef
Nilstrieb <48135649+Nilstrieb@users.noreply.github.com>
2022-04-05 19:42:10 +0200

    adaptors

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,14 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
-    }
-}
+#![no_std]
+#![warn(rust_2018_idioms)]
+#![deny(unsafe_op_in_unsafe_fn)]
+
+mod strategy;
+
+pub use stuff::Backend;
+
+use crate::strategy::{SafeStrategyAdaptor, SafeStuffingStrategy};
+
+pub struct StuffedPtr<T, S, B = usize>(stuff::StuffedPtr<T, SafeStrategyAdaptor<S>, B>)
+where
+    S: SafeStuffingStrategy<B>,
+    B: Backend<T>;
